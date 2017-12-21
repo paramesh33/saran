db95ff21d67e49cb94dd15b14877c4c5ac095537
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2017-12-21 09:48:51 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a;
+ printf("enter the number a:");
+ scanf("%d",&a);
+ if(a%2==0)
+ { 
+ printf("\nthe number is even")
+ }
+ else
+ printf("\nthe number is odd");
+ getch();
+ }
