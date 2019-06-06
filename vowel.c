b42b9d5eced36677d59c6e218906bcd0cc48628e
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2019-06-06 11:26:37 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,14 +3,12 @@
 void main()
 {
  char letter;
- clrscr();
- printf("enter the value:");
  scanf("%c",&letter);
  if(letter=='A'||letter=='E'||letter=='I'||letter=='O'||letter=='U'||letter=='a'||letter=='e'||letter=='o'||letter=='i'||letter=='u')
  {
-  printf("\nthe letter is vowel");
+  printf("\nVowel");
  }
  else
-  printf("\nthe letter is consonant");
+  printf("\nConsonant");
   getch();
   }
